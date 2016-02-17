f404b73f7adad17e1c906d650d8cb2d2d0570ac0
root <root@sc1ansiblesrv.pdnt.blackbaud.com>
2016-02-17 17:25:46 -0500

    wrap invoke command

--- roles/crmsqldeploy/scripts/DeployCrmDatabase.ps1
+++ roles/crmsqldeploy/scripts/DeployCrmDatabase.ps1
@@ -1,4 +1,4 @@
-﻿<# SQL Server Prep and Revisions process
+<# SQL Server Prep and Revisions process
 This script:
     1. copies the build to the server
     2. Backs the target database up (compress), and msdb to preserve jobs
@@ -12,112 +12,131 @@ Usage:
 [CmdletBinding()]
 param
 (
-    [Parameter(Mandatory=$true)][string]$buildDropLocation, 
+    [Parameter(Mandatory=$true)][string]$buildDropLocation,
     [Parameter(Mandatory=$true)][string]$bakFilePath,
-	[Parameter(Mandatory=$true)][string]$databaseName
+    [Parameter(Mandatory=$true)][string]$databaseName
 )
 
+$buildDropLocation = $buildDropLocation.replace("/", "\")
+$bakFilePath = $bakFilePath.replace("/", "\")
 
-Import-Module "\\ptlserver9\CrmPowershell\DEBUG\CRMManifest.psd1"
-Import-module "\\ptlserver9\CrmPowershell\sdo\Powershell\SDOManifest.psd1"
+scriptblock = {
+param
+(
+    [Parameter(Mandatory=$true)][string]$buildDropLocation, 
+    [Parameter(Mandatory=$true)][string]$bakfilePath,
+    [Parameter(Mandatory=$true)][string]$databaseName
+
+)
+    Import-Module "\\ptlserver9\CrmPowershell\DEBUG\CRMManifest.psd1"
+    Import-module "\\ptlserver9\CrmPowershell\sdo\Powershell\SDOManifest.psd1"
 
 
-# This machine is the SQL host
-$SQLInstance = $env:COMPUTERNAME
+    # This machine is the SQL host
+    $SQLInstance = $env:COMPUTERNAME
 
-$currentUser = [Environment]::UserDomainName + "\" + [Environment]::UserName
-Write-Host "Currently running as $currentUser"
+    $currentUser = [Environment]::UserDomainName + "\" + [Environment]::UserName
+    Write-Host "Currently running as $currentUser"
 
-# -- Determine if patch build --
-$isPatchBuild = $buildDropLocation.Contains("Patches")
-if ($isPatchBuild) {
-    $patchDropLocation = $buildDropLocation
-    $buildDropLocation = $patchDropLocation.Substring(0, $patchDropLocation.IndexOf("Patches") - 1)
-    
-    Write-Host "Installing build from $buildDropLocation"
-    Write-Host "Installing patch from $patchDropLocation"
-}
+    # -- Determine if patch build --
+    $isPatchBuild = $buildDropLocation.Contains("Patches")
+    if ($isPatchBuild) {
+        $patchDropLocation = $buildDropLocation
+        $buildDropLocation = $patchDropLocation.Substring(0, $patchDropLocation.IndexOf("Patches") - 1)
 
-# Set all parameters for the installation
-$Product = "Phoenix"
-$vDir = "bbappfx_$Product"
-$baseInstallDir = "c:\Infinity\$Product"
+        Write-Host "Installing build from $buildDropLocation"
+        Write-Host "Installing patch from $patchDropLocation"
+    }
 
-if (!(Test-Path -Path $baseInstallDir)) {
-    Write-Host "Creating folder $baseInstallDir"
-    New-Item -Path $baseInstallDir -ItemType Directory | Out-Null
-}
+    # Set all parameters for the installation
+    $Product = "Phoenix"
+    $vDir = "bbappfx_$Product"
+    $baseInstallDir = "c:\Infinity\$Product"
 
-# -- Install the application -- 
-Write-Host "Installing Build from $buildDropLocation to $baseInstallDir"
-Install-CRMComponents -sourceDir $buildDropLocation -destDir $baseInstallDir
+    if (!(Test-Path -Path $baseInstallDir)) {
+        Write-Host "Creating folder $baseInstallDir"
+        New-Item -Path $baseInstallDir -ItemType Directory | Out-Null
+    }
 
-if ($isPatchBuild) {
-    Write-Host "Installing Patch from $patchDropLocation"
-    Install-CRMPatch -patchDir $patchDropLocation -baseInstallDir $baseInstallDir
-}
+    # -- Install the application --
+    Write-Host "Installing Build from $buildDropLocation to $baseInstallDir"
+    Install-CRMComponents -sourceDir $buildDropLocation -destDir $baseInstallDir
 
-$instance = Get-SDOSQLServerInstances -ComputerName $env:COMPUTERNAME
-$databasepresent = $instance.ConnectionString | Get-SDOSQLServerDatabase -database $databaseName
-$primaryInstanceName = $($instance.Hostname + "\" + $instance.InstanceName  + "," + $instance.PortNumber)
+    if ($isPatchBuild) {
+        Write-Host "Installing Patch from $patchDropLocation"
+        Install-CRMPatch -patchDir $patchDropLocation -baseInstallDir $baseInstallDir
+    }
 
-Write-Host "Starting database installation for db $databaseName"
+    $instance = Get-SDOSQLServerInstances -ComputerName $env:COMPUTERNAME
+    $databasepresent = $instance.ConnectionString | Get-SDOSQLServerDatabase -database $databaseName
+    $primaryInstanceName = $($instance.Hostname + "\" + $instance.InstanceName  + "," + $instance.PortNumber)
 
-# does the target db already exist? If so, revision it, it not, mount it
-if ($databasepresent) {
-# just run revisions
-    # back it up so can revert from revision failure
-    Write-Host "Database $databaseName already present, only revisions will be run"
-    Write-Host "Backing up database before revisions."
-    $tmpBakFile = Backup-CRMSqlDatabase -SQLInstance $primaryInstanceName -DatabaseName $databaseName -Initialize
+    Write-Host "Starting database installation for db $databaseName"
 
-    try {
-        Update-CRMRevisions -installDir $baseInstallDir -DatabaseName $databaseName -SQLInstance $primaryInstanceName 
+    # does the target db already exist? If so, revision it, it not, mount it
+    if ($databasepresent) {
+    # just run revisions
+        # back it up so can revert from revision failure
+        Write-Host "Database $databaseName already present, only revisions will be run"
+        Write-Host "Backing up database before revisions."
+        $tmpBakFile = Backup-CRMSqlDatabase -SQLInstance $primaryInstanceName -DatabaseName $databaseName -Initialize
+
+        try {
+            Update-CRMRevisions -installDir $baseInstallDir -DatabaseName $databaseName -SQLInstance $primaryInstanceName
+            }
+        catch {
+            write-host "Revisions failed, restoring from backup!"
+            Restore-CRMDatabase -databaseName $databaseName -SQLInstance $primaryInstanceName -databasePath $tmpBakFile -ReplaceDatabase
+            throw $_.Exception
         }
-    catch {
-        write-host "Revisions failed, restoring from backup!"
-        Restore-CRMDatabase -databaseName $databaseName -SQLInstance $primaryInstanceName -databasePath $tmpBakFile -ReplaceDatabase
-        throw $_.Exception
+    }
+    else {
+        Write-Host "Database $databaseName not found, installing new db $databaseName"
+        # Move database backup
+        $localDbBackup = "$baseInstallDir\local.bak"
+        Write-Host "Copying database backup from $bakFilePath to $localDbBackup"
+        Copy-Item -Path $bakFilePath -Destination $localDbBackup -Force
+
+        # Attach and prep the DB - assumption is database coming from a parameter
+        Write-Host "Loading database $databaseName from backup $localDbBackup"
+        Restore-CRMDatabase -databaseName $databaseName -SQLInstance $primaryInstanceName -databasePath $localDbBackup -ReplaceDatabase
+
+         # set to full backup
+        Set-CRMDBRecoveryModel -DatabaseName $databaseName -SQLInstance $primaryInstanceName -RecoveryModel 'Full'
+
+        Write-Host "Updating encryption keys"
+        Set-CRMEncryptionKey -installDir $baseInstallDir -DatabaseName $databaseName -SQLInstance $primaryInstanceName
+
+        Write-Host "Running revisions"
+        Update-CRMRevisions -installDir $baseInstallDir -DatabaseName $databaseName -SQLInstance $primaryInstanceName
+
+        # -- Setup BBPS --
+        Write-Host "Adding BBPS account to $databaseName on $primaryInstanceName"
+        Add-CRMBBPSAccount -databaseName $databaseName -SQLInstance $primaryInstanceName
+
+        # Setup lab user on the SQL server
+        Write-Host "Adding system user to database $databaseName"
+        Add-CRMSystemUser -databaseName $databaseName -SQLInstance $primaryInstanceName -ComputerName $SQLInstance
+
+        # Setup lab user on the SQL server
+        Write-Host "Adding Job User Role to $databaseName"
+        Add-CRMJobUserRole -installDir $baseInstallDir -ComputerName $SQLInstance -DatabaseName $databaseName -SQLInstance $primaryInstanceName
+
+        Write-Host "Adding Job User to $databaseName"
+        Add-CRMJobUser -installDir $baseInstallDir -DatabaseName $databaseName -SQLInstance $primaryInstanceName
+
+        Write-Host "Adding Proxy rights to system user"
+        Add-CRMJobUserProxyRights -installDir $baseInstallDir -ComputerName $SQLInstance -DatabaseName $databaseName -SQLInstance $primaryInstanceName
+
+        Write-Host "Installing new db $databaseName complete."
+
     }
 }
-else { 
-    Write-Host "Database $databaseName not found, installing new db $databaseName"
-    # Move database backup 
-    $localDbBackup = "$baseInstallDir\local.bak"
-    Write-Host "Copying database backup from $bakFilePath to $localDbBackup"
-    Copy-Item -Path $bakFilePath -Destination $localDbBackup -Force
 
-    # Attach and prep the DB - assumption is database coming from a parameter
-    Write-Host "Loading database $databaseName from backup $localDbBackup"
-    Restore-CRMDatabase -databaseName $databaseName -SQLInstance $primaryInstanceName -databasePath $localDbBackup -ReplaceDatabase
+$user = 'pdnt\automagic'
+$pass = ConvertTo-SecureString -String "Research6" -AsPlainText -Force
+$creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($user, $pass)
 
-     # set to full backup
-    Set-CRMDBRecoveryModel -DatabaseName $databaseName -SQLInstance $primaryInstanceName -RecoveryModel 'Full'
+$session = New-PSSession -ComputerName $SQLInstance -Credential $creds -Authentication Credssp
 
-    Write-Host "Updating encryption keys"
-    Set-CRMEncryptionKey -installDir $baseInstallDir -DatabaseName $databaseName -SQLInstance $primaryInstanceName
-
-    Write-Host "Running revisions" 
-    Update-CRMRevisions -installDir $baseInstallDir -DatabaseName $databaseName -SQLInstance $primaryInstanceName 
-
-    # -- Setup BBPS --
-    Write-Host "Adding BBPS account to $databaseName on $primaryInstanceName"
-    Add-CRMBBPSAccount -databaseName $databaseName -SQLInstance $primaryInstanceName
-
-    # Setup lab user on the SQL server
-    Write-Host "Adding system user to database $databaseName"
-    Add-CRMSystemUser -databaseName $databaseName -SQLInstance $primaryInstanceName -ComputerName $SQLInstance
-
-    # Setup lab user on the SQL server
-    Write-Host "Adding Job User Role to $databaseName"
-    Add-CRMJobUserRole -installDir $baseInstallDir -ComputerName $SQLInstance -DatabaseName $databaseName -SQLInstance $primaryInstanceName
-
-    Write-Host "Adding Job User to $databaseName"
-    Add-CRMJobUser -installDir $baseInstallDir -DatabaseName $databaseName -SQLInstance $primaryInstanceName 
-
-    Write-Host "Adding Proxy rights to system user"
-    Add-CRMJobUserProxyRights -installDir $baseInstallDir -ComputerName $SQLInstance -DatabaseName $databaseName -SQLInstance $primaryInstanceName
-
-    Write-Host "Installing new db $databaseName complete."
-
-}
+Invoke-Command -Session $session -ScriptBlock $bigscriptblock -ArgumentList $buildDropLocation,$bakFilePath,$databaseName
