fca72c742a26b489b67c8d4d3dcb793dd4e6c656
root <root@sc1ansiblesrv.pdnt.blackbaud.com>
2016-02-22 22:34:29 -0500

    new role for bulk insert

---
+++ roles/deploypowershell/scripts/win_copyfiles.ps1
@@ -0,0 +1,75 @@
+param
+(
+    [string]$srcFolder,
+    [string]$destFolder,
+    [string]$user,
+    [string]$pass
+)
+
+$scriptblock = {
+    param
+    (
+        [string]$srcFolder,
+        [string]$destFolder
+    )
+
+    Function Copy-SourceToTarget {
+        [cmdletbinding()]
+        Param
+        (
+            [string]$source,
+            [string]$target,
+            [string]$exclusions
+        )
+
+        [string]$tempLog = [System.IO.Path]::GetTempFileName()
+
+        if ([String]::IsNullOrEmpty($exclusions)) {
+            robocopy $source $target /S /MT /NFL /NDL /NJH /NJS /NC /NS /NP /log:$tempLog
+        }
+        else {
+            robocopy $source $target /S /MT /NFL /NDL /NJH /NJS /NC /NS /NP /XF $exclusions /log:$tempLog
+        }
+    }
+
+    Function Copy-CRMMirrorSourceAndTarget {
+        [cmdletbinding()]
+        Param
+        (
+            [string]$source,
+            [string]$target,
+            [string]$exclusions
+        )
+
+        [string]$tempLog = [System.IO.Path]::GetTempFileName()
+
+        if ([String]::IsNullOrEmpty($exclusions)) {
+            robocopy $source $target /MIR /MT /NFL /NDL /NJH /NJS /NC /NS /NP /log:$tempLog
+        }
+        else {
+            Write-Host "With exclusions"
+            robocopy $source $target /MIR /MT /NFL /NDL /NJH /NJS /NC /NS /NP /XF $exclusions /log:$tempLog
+        }
+    }
+
+    if ((Get-Item $srcFolder) -is [System.IO.DirectoryInfo]) {
+        [boolean]$upgrade = [System.IO.Directory]::Exists($destFolder)
+        if ($upgrade) {
+            Copy-CRMMirrorSourceAndTarget -source $srcFolder -target $destFolder -exclusions $null
+        } else {
+            Copy-SourceToTarget -source $srcFolder -target $destFolder -exclusions $null
+        }
+    }
+    else {
+        Copy-Item "$srcFolder" -Destination $destFolder -Force
+    }
+}
+
+
+$secpasswd = ConvertTo-SecureString $pass -AsPlainText -Force
+$creds = New-Object System.Management.Automation.PSCredential ($user, $secpasswd)
+
+$session = New-PSSession -ComputerName $env:Computername -Credential $creds -Authentication Credssp
+
+Invoke-Command -Session $session -ScriptBlock $scriptblock -ArgumentList $srcFolder,$destFolder
+
---
+++ roles/sdovmprep/scripts/webserverprep.ps1
@@ -0,0 +1,31 @@
+﻿[CmdletBinding()]
+param
+(
+    [Parameter(Mandatory=$true)][string]$hostname, 
+    [Parameter(Mandatory=$true)][string]$configpath,
+    [Parameter(Mandatory=$true)][string]$websitename
+)
+
+$configpath = $configpath.replace("/", "\")
+
+$scriptblock = {
+param
+(
+    [string]$hostname, 
+    [string]$configpath,
+    [string]$websitename
+
+)
+
+    import-module "\\ptlserver9\CrmPowershell\SDO\Powershell\SDOManifest.psd1"
+
+    Initialize-SDOWebServer -ComputerName $hostname -ServerSetupConfigFilePath $configpath -WebSiteName $websitename
+}
+
+$user = 'pdnt\automagic'
+$pass = ConvertTo-SecureString -String "Research6" -AsPlainText -Force
+$creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($user, $pass)
+
+$session = New-PSSession -ComputerName $env:Computername -Credential $creds -Authentication Credssp
+
+Invoke-Command -Session $session -ScriptBlock $scriptblock -ArgumentList $hostname,$configpath,$websitename
